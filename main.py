93e05d8537302a80059b4a0df48c9d75415b8cd2
Ivan Holikov <laurence4312@gmail.com>
2021-02-16 11:49:55 +0200

    Added 'process_killing' method

--- main.py
+++ main.py
@@ -1,4 +1,24 @@
+import psutil
+
+bad_process_names = {
+                    'vmms.exe',
+                    'com.docker.service',
+                    'vmcompute.exe',
+                    'SkypeBackgroundHost.exe',
+                    'SkypeApp.exe',
+}
+
+
+def process_killing(process_names_collection):
+    for proc in psutil.process_iter(['pid', 'name']):
+        if proc.info['name'] in process_names_collection:
+            p = psutil.Process(proc.info['pid'])
+            try:
+                p.kill()
+            except psutil.AccessDenied:
+                print("Process " + proc.info['name'] + " (pid=" + str(proc.info['pid']) +
+                      ") was not killed (psutil.AccessDenied).")
 
 
 if __name__ == '__main__':
-    print('PyCharm')
+    process_killing(bad_process_names)
